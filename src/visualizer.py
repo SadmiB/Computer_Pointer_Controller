4d47355d29cc8712a33ec25b0f02a56e582b8aeb
SadmiB <sadmibouhafs@gmail.com>
2020-07-17 16:47:30 +0100

    Yay! it visualizing head pose

--- src/visualizer.py
+++ src/visualizer.py
@@ -38,21 +38,38 @@ class Visualizer:
         cv2.arrowedLine(self.face, (rx, ry), (rx + gx, ry + gy), (0,255,255), 1)
 
 
-
-
-        
-
     def draw_head_pose(self):
         
         yaw = self.head_pose[0] * math.pi / 180 
         pitch = self.head_pose[1] * math.pi / 180 
         roll = self.head_pose[2] * math.pi / 180 
 
+        sin_yaw = math.sin(yaw)
+        sin_pitch = math.sin(pitch)
+        sin_roll = math.sin(roll)
 
-        cv2.putText(self.face, "yaw={:.3f}\n pitch={:.3f}, roll={:.3f}"\
-            .format(yaw, pitch, roll), (5, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 1)
+        cos_yaw = math.cos(yaw)
+        cos_pitch = math.cos(pitch)
+        cos_roll = math.cos(roll)
 
-        #cv2.line(self.face, (cx, cy), ())
+
+        axis_length = 0.5 * self.face.shape[1]
+
+        lx, ly, rx, ry = self.eyes_landmarks
+
+        center = (int((lx + rx)/2),  int((ly + ry)/2))
+      
+        cv2.arrowedLine(self.face, center, (int(center[0] + axis_length * (cos_roll * cos_yaw + sin_yaw * sin_pitch * sin_roll)),\
+             int(center[1] + axis_length * cos_pitch * sin_roll)), (0,0,255), 1)
+
+        cv2.arrowedLine(self.face, center, (int(center[0] + axis_length * (cos_roll * sin_yaw * sin_pitch + cos_yaw * sin_roll)),\
+             int(center[1] - axis_length * cos_pitch * cos_roll)), (0,255,0), 1)
+
+        cv2.arrowedLine(self.face, center, (int(center[0] + axis_length * sin_yaw * cos_pitch),\
+             int(center[1] + axis_length * sin_pitch)), (255,0,0), 1)
+
+        cv2.putText(self.face, "yaw={:.3f}, pitch={:.3f}, roll={:.3f}"\
+            .format(yaw, pitch, roll), (5, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (255, 0, 0), 1)
         
 
 
